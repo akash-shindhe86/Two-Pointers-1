2bd0c99cb14c519a20b0cb59bf741fa165793442
ashin19 <ashin19@safeway.com>
2024-08-12 06:11:04 -0700

    Done

--- Problem-2.java
+++ Problem-2.java
@@ -1,29 +1,45 @@
-// Time Complexity : O(m+n)
-// Space Complexity : O(1) I have not used any extra space.
+// Time Complexity : O(nlogn)(sorting) + O(n2) = O(n2logn)
+// Space Complexity : O(1) I used output array.
 // Did this code successfully run on Leetcode : Yes
 // Three line explanation of solution in plain english: I used the same logic Jaspinder explained. 
 
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m - 1;
-        int p2 = n - 1;
-        int idx = m + n - 1;
+    public List<List<Integer>> threeSum(int[] nums) {
+        // int outrP = 0;
+        // int l=0,r=nums.length - 1;
+        List<List<Integer>> list = new ArrayList<>();
 
-        while(p1 >= 0 && p2 >= 0){
-            if(nums1[p1] >= nums2[p2]){
-                nums1[idx] = nums1[p1];
-                p1--;
-            }else{
-                nums1[idx] = nums2[p2];
-                p2--;
+        Arrays.sort(nums);
+
+        for(int i = 0;i < nums.length; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            if(nums[i] > 0) break;
+
+            int low = i + 1;
+            int high = nums.length - 1;
+            
+            while(low < high){
+                int total = nums[i] + nums[low] + nums[high];
+                if(total == 0){
+                    List<Integer> triplet = Arrays.asList(nums[i],nums[high],nums[low]);
+                    list.add(triplet);
+                    low++;
+                    high--;
+
+                    //inner duplicacy
+                    while(low< high && nums[low] == nums[low -1]){
+                        low++;
+                    }
+                    while(low < high && nums[high] == nums[high + 1]){
+                        high--;
+                    }
+                }else if(total < 0){
+                    low++;
+                }else{
+                    high--;
+                }
             }
-            idx--;
         }
-
-        while(p2>= 0){
-            nums1[idx] = nums2[p2];
-            p2--;
-            idx--;
-        }
+        return list;
     }
 }
